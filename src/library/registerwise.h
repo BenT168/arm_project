7d693a22b2bcda581441ed4b953a2ad15839a495
Ching Wong <ching.wong15@imperial.ac.uk>
2016-05-24 00:47:12 +0100

    Trying to fix the WRITE_REG function

--- src/library/registerwise.h
+++ src/library/registerwise.h
@@ -17,10 +17,10 @@ int32_t as_immediate_reg(int value);
 
 ///////////////////////////// MACROS /////////////////////////////////////////
 
-
 /* REGISTER READ/WRITE */
 #define REG_READ(r)     (arm_Ptr->registers[(r)])
 #define REG_WRITE(r, v) (arm_Ptr->registers[(r)] = (v))
+
 
 /* CPSR READ/WRITE */
 #define CPSR_CLR(posf)    (BIT_CLR(REG_READ(CPSR), (posf)))
