56bbc8c6ab4b4fe673a0f9aebc3841fdd401cce0
Adanna <aa14415@ic.ac.uk>
2016-05-25 21:11:07 +0100

    Did main, write file and read file in assemble.c
    Added tokens.c tokens.h, assembler.h in library

---
+++ src/library/assembler.h
@@ -0,0 +1,12 @@
+#ifndef ASSEMBLER
+#define ASSEMBLER
+
+#include <stdint.h>
+
+typedef struct ASSEMBLER_STRUCT {
+  char *symbolTable //array of symbol table
+  int TOTAL_line   // total of the line
+} ASSEMBLER_STRUCT;
+
+
+#endif
